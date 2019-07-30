9b95b018e857c04ced6a12ba9e85e935f2d0bfa6
Rahul Dubey <rahul@roadzen.io>
2019-07-30 11:42:32 +0530

    build

---
+++ build/precache-manifest.4a34cbb6a48ed02cf6fdaed8163a3e07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b41e715393c75bef4c6decfae9eeb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "940b8d905752b5750535",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "940b8d905752b5750535",
+    "url": "/static/js/main.49865d35.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
